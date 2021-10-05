b3516fa6fba0f280dbd5aaaf3645777ce3c4c8f3
Derek <dmfranz@wisc.edu>
2021-10-05 15:51:56 -0500

    More work on the create account system

---
+++ register/views.py
@@ -0,0 +1,19 @@
+from django.http import response
+from django.shortcuts import render, redirect
+from django.contrib.auth import login, authenticate
+from django.contrib.auth.forms import UserCreationForm
+
+# Create your views here.
+
+def register(request):
+    if request.method == "POST":
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            form.save()
+
+        return redirect('/spikeapp')
+    else:
+        form = UserCreationForm()
+    
+
+    return render(request, 'register/register.html', {'form': form})
