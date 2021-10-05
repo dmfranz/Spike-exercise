bee4764b10fb0fbc4685eff47e8874f2b4d126f5
Derek <dmfranz@wisc.edu>
2021-10-04 19:27:22 -0500

    made a test site with a simple test database

--- spikeapp/models.py
+++ spikeapp/models.py
@@ -1,3 +1,8 @@
 from django.db import models
+from django.db.models.base import ModelState
 
 # Create your models here.
+class Login(models.Model):
+    username = models.CharField(max_length=100)
+    password = models.CharField(max_length=500)
+    
--- spikeapp/views.py
+++ spikeapp/views.py
@@ -1,6 +1,11 @@
 from django.shortcuts import render
+from spikeapp.models import Login
 
 # Create your views here.
 
 def spikeapp(request):
-    return render(request, 'helloworld.html',{})
+    logins = Login.objects.all()
+    context = {
+        'logins': logins
+    }
+    return render(request, 'spike_app_index.html',context=context)
