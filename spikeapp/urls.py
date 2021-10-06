20667ab538c8ab96b0d5459c9e5af4b3dc860ae0
MirroredNova <anethercrafter@gmail.com>
2021-10-06 00:51:11 -0500

    work done on homepage

--- spikeapp/urls.py
+++ spikeapp/urls.py
@@ -3,5 +3,4 @@ from spikeapp import views
 
 urlpatterns = [
     path('', views.spikeapp, name='spikeapp'),
-    path('createaccount/',views.createaccount, name='createaccount')
 ]
--- spikeapp/views.py
+++ spikeapp/views.py
@@ -10,27 +10,3 @@ def spikeapp(request):
         'logins': logins
     }
     return render(request, 'spike_app_index.html',context=context)
-
-# TODO
-# 1) Check if this user already exists
-# 2) Decide some action on failure
-def createaccount(request):
-    if request.method == "POST":
-        form = CreateNewAccount(request.POST)
-
-        if form.is_valid():
-            username = form.cleaned_data['username']
-            password = form.cleaned_data['password']
-            re_entered_password = form.cleaned_data['re_entered_pass']
-
-            # Does nothing if the passwords dont match for the time being
-            if password == re_entered_password:
-                # encrypt the password here
-                login = Login(username=username, password=password)
-                login.save()
-
-        return response.HttpResponseRedirect("..")
-    else:
-        form = CreateNewAccount()
-    
-    return render(request, "createaccount.html",{"form":form})
