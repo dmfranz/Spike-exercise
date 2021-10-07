c9361ac15c01edc327d203418db15e0dcdcc420b
BMarmara <bmarmara13@gmail.com>
2021-10-07 14:39:34 -0500

    add requests.html for creating maintenance request

--- spikeapp/models.py
+++ spikeapp/models.py
@@ -18,3 +18,17 @@ class RentalApplication(models.Model):
 class User(models.Model):
     username = models.CharField(max_length=200)
     is_renter = models.BooleanField(default=False)
+
+# PRIORITY_CHOICES = (
+#     ('urgent','URGENT'),
+#     ('low priority', 'LOW'),
+# )
+
+# class Priority(models.Model):
+#     priority = models.CharField(max_length=2, choices=PRIORITY_CHOICES, default='low priority')
+
+class RequestForm(models.Model):
+    tenant_name = models.CharField(max_length=100)
+    landlord_name = models.CharField(max_length=100)
+    message = models.CharField(max_length=500)
+    priority = models.CharField(max_length=100)
