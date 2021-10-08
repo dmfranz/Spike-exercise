b284f394746e8640362025d96bfc4de289f18b87
MirroredNova <anethercrafter@gmail.com>
2021-10-08 03:54:14 -0500

    formatting fixes

--- spikeapp/forms.py
+++ spikeapp/forms.py
@@ -5,7 +5,6 @@ from spikeapp.cardhandling import CreditCardField
 from spikeapp.cardhandling import CreditCardExpirationField
 
 
-
 class CreateNewRentalApplication(forms.Form):
     first_name = forms.CharField(label='First Name', max_length=200)
     last_name = forms.CharField(label='Last Name', max_length=200)
@@ -13,17 +12,18 @@ class CreateNewRentalApplication(forms.Form):
     phone_number = forms.IntegerField(label='Phone number')
 
     
-PRIORITY_CHOICES= [
+PRIORITY_CHOICES = [
     ('low', 'Low Priority'),
     ('high', 'Urgent'),
 ]
 
 
 class CreateRequestForm(forms.Form):
-    tenant_name= forms.CharField(label='Full Name', max_length=100)
-    landlord_name= forms.CharField(label='Landlord Name', max_length=100)
-    message= forms.CharField(label='Please enter your maintenance request here', widget=forms.Textarea(attrs={'style': "width:80%;"}), max_length=500)
-    priority= forms.CharField(label='What is the priority?', widget=forms.Select(choices=PRIORITY_CHOICES))
+    tenant_name = forms.CharField(label='Full Name', max_length=100)
+    landlord_name = forms.CharField(label='Landlord Name', max_length=100)
+    message = forms.CharField(label='Please enter your maintenance request here', widget=forms.Textarea(attrs={'style': "width:80%;"}), max_length=500)
+    priority = forms.CharField(label='What is the priority?', widget=forms.Select(choices=PRIORITY_CHOICES))
+
 
 class MakePayment(ModelForm):
     class Meta:
